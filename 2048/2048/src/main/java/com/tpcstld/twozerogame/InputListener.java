dfa8da2b67853edd7cbaafe03db3245122b965ff
buddybuild <team@buddybuild.com>
2017-07-12 15:59:54 +0000

    Fix crash

--- 2048/2048/src/main/java/com/tpcstld/twozerogame/InputListener.java
+++ 2048/2048/src/main/java/com/tpcstld/twozerogame/InputListener.java
@@ -130,11 +130,6 @@ class InputListener implements View.OnTouchListener {
                         }
                     } else if (iconPressed(mView.sXUndo, mView.sYIcons)) {
                         mView.game.revertUndoState();
-                        try {
-                            throw new RuntimeException("Original Exception");
-                        } catch (RuntimeException e) {
-                            throw new RuntimeException("Check out buddybuild Crash Reporting!");
-                        }
                     } else if (isTap(2) && inRange(mView.startingX, x, mView.endingX)
                             && inRange(mView.startingY, x, mView.endingY) && mView.continueButtonEnabled) {
                         mView.game.setEndlessMode();
